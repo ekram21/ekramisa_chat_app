d0bd6ef9055cb22268ac2e849170b400891626c9
Ekram <ekram_lol@hotmail.com>
2018-09-12 22:40:57 +0600

    final commit

--- screens/Chat.js
+++ screens/Chat.js
@@ -22,7 +22,8 @@ export default class Chat extends React.Component {
     static navigationOptions = ({ navigation }) => ({ title: navigation.state.params.title || 'a',});
 
     state = {
-        innerMessage: '',
+        messageWriterArr: [],
+        innerMessageArr: [],
         keyboardDisplayed: false,
     }
 
@@ -33,7 +34,19 @@ export default class Chat extends React.Component {
         console.log('UserName: ' + UserName);
         this.props.navigation.setParams({ title: UserName });
 
+        //set up polling updater to check the keyboard display state and change the text box position accordingly
         this.keyboardDidHideListener = Keyboard.addListener('keyboardDidHide', this._keyboardDidHide);
+
+        //set up the new message polling updater
+        let OwnerUID = firebase.auth().currentUser.uid;
+        let OwnerEmail = firebase.auth().currentUser.email;
+
+        let FriendUID = this.props.navigation.getParam('clickedFriendUID');
+
+        let ownRoomID = 'MessageRoom' + OwnerUID + FriendUID;
+
+        let address = 'USERS/' + OwnerUID + '/' + EncodeString(OwnerEmail) + '/' + ownRoomID + '/';
+        this.AutoUpdater(address);
     };
 
     inputFocused(){
@@ -56,6 +69,7 @@ export default class Chat extends React.Component {
         console.log('send message pressed!');
 
         //access the firebase database and store this message
+        //first get all the relevant variables
         let messageToSend = this.state.innerMessage;
 
         let OwnerUID = firebase.auth().currentUser.uid;
@@ -64,51 +78,17 @@ export default class Chat extends React.Component {
         let FriendUID = this.props.navigation.getParam('clickedFriendUID');
         let FriendEmail = this.props.navigation.getParam('clickedFriendEmail');
 
-        let messageWriter = EncodeString(OwnerEmail);
+        //now send the message
+        _SendMessage(OwnerUID, OwnerEmail, FriendUID, FriendEmail, messageToSend);
 
-        let d = new Date();
-        d.toUTCString();
+    }
 
-        let onlyTime = String(d).split(' ')[4];
-
-        let ownRoomID = 'MessageRoom' + OwnerUID + FriendUID;
-
+    AutoUpdater(address) {
+        //this function will keep checking to see if new child message has been added to the database for this address
         let database = firebase.database();
+        let ref = database.ref(address);
 
-        let ref = database.ref('USERS/' + OwnerUID + '/' + EncodeString(OwnerEmail) + '/' + ownRoomID + '/');
-
-        let data = {};
-
-        data[messageWriter] = {};
-
-        data[messageWriter] = {
-            [onlyTime]: messageToSend
-        };
-
-        ref.push(data).then(function(){
-            console.log('successfully sent message..');
-
-            //now write this message in friends database
-            let friendRoomID = 'MessageRoom' + FriendUID + OwnerUID;
-
-            let database = firebase.database();
-
-            let ref = database.ref('USERS/' + FriendUID + '/' + EncodeString(FriendEmail) + '/' + friendRoomID + '/');
-
-            let data = {};
-
-            data[messageWriter] = {};
-
-            data[messageWriter] = {
-                [onlyTime]: messageToSend
-            };
-
-            ref.push(data);
-
-            alert('data sent!');
-
-        })
-
+        ref.limitToLast(1).on("child_added", function (snapshot, previousChildKey) { HandleNewMessageEvent("child_added", snapshot, previousChildKey) });
     }
 
     renderCurrentState(){
@@ -206,7 +186,6 @@ export default class Chat extends React.Component {
 
                         <ChatMessage label={'hi'} rightOrLeft={'flex-start'}></ChatMessage>
 
-
                     </ScrollView>
 
 
@@ -269,3 +248,88 @@ const styles = StyleSheet.create({
         backgroundColor: 'white' 
     }
 })
+
+function _PullAllMessages(address){
+
+    let database = firebase.database();
+    let ref = database.ref(address);
+
+}
+
+function _SendMessage(ownerUID, ownerMail, friendUID, friendMail, innerMessage){
+
+    //firebase function to send message
+
+    let messageToSend = EncodeString(innerMessage);
+
+    let OwnerUID = ownerUID;
+    let OwnerEmail = ownerMail;
+
+    let FriendUID = friendUID;
+    let FriendEmail = friendMail;
+
+    let messageWriter = EncodeString(OwnerEmail);
+
+    let d = new Date();
+    d.toUTCString();
+
+    let onlyTime = String(d).split(' ')[4];
+
+    let ownRoomID = 'MessageRoom' + OwnerUID + FriendUID;
+
+    let database = firebase.database();
+
+    let ref = database.ref('USERS/' + OwnerUID + '/' + EncodeString(OwnerEmail) + '/' + ownRoomID + '/');
+
+    let data = {};
+
+    data[messageWriter] = {};
+
+    data[messageWriter] = {
+        [onlyTime]: messageToSend
+    };
+
+    ref.push(data).then(function () {
+        console.log('successfully sent message..');
+
+        //now write this message in friends database
+        let friendRoomID = 'MessageRoom' + FriendUID + OwnerUID;
+
+        let database = firebase.database();
+
+        let ref = database.ref('USERS/' + FriendUID + '/' + EncodeString(FriendEmail) + '/' + friendRoomID + '/');
+
+        let data = {};
+
+        data[messageWriter] = {};
+
+        data[messageWriter] = {
+            [onlyTime]: messageToSend
+        };
+
+        ref.push(data);
+
+    })
+}
+
+function HandleNewMessageEvent(event, snapshot, optionalPreviousChildKey) {
+    //make the new message box in receive chat box
+
+    //now break it into useful components before injecting it into the chat receive box
+    messageWriter = '';
+    innerMessage = '';
+
+    let key;
+    for (key in snapshot.val()){
+        messageWriter = DecodeString(key);
+
+        let key2;
+        for (key2 in snapshot.val()[key]){
+            innerMessage = DecodeString(snapshot.val()[key][key2]);
+        }
+    }
+
+    console.log(messageWriter);
+    console.log(innerMessage);
+
+}
